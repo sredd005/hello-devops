24a196e573230198f27a112d6a0ba7db782494b3
Saikrishna Reddy <35349978+sredd005@users.noreply.github.com>
2025-08-22 17:03:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps v2 from Salesforce Mini-Platform!"
+    return "Hello DevOps v2 from Salesforce Mini-Platform!I like dosa."
 
 @app.route("/healthz")
 def healthz():
